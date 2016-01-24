148e0a0385918d21c6147dbfe79776f73f9918c7
R. Alex Clark <aclark2089@gmail.com>
2016-01-23 19:29:20 -0500

    Setup main screen

---
+++ Friend Circle/FriendCircleCell.swift
@@ -0,0 +1,13 @@
+//
+//  FriendCircleCell.swift
+//  Friend Circle
+//
+//  Created by Alex Clark on 1/23/16.
+//  Copyright © 2016 Nicholas Miller. All rights reserved.
+//
+
+import UIKit
+
+class FriendCircleCell: UICollectionViewCell {
+    
+}
